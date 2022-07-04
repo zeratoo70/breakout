2dbc02861c6dac3dc397763d426308fca2b61d8c
Zeratoo70 <Zeratoo70@gmail.com>
2022-07-04 23:34:32 +0200

    Release commit

--- agent.py
+++ agent.py
@@ -13,67 +13,63 @@ class ReplayBuffer:
     """ReplayBuffer используется для хранения предыдущего опыта, для того чтобы агент учился на нем
     
     """
-    def __init__(self, capacity: int = 100000) -> None:
+    def __init__(self, device, capacity: int = int(1e6)) -> None:
         self.memory = deque([], maxlen=capacity)
+        self.device = device
     
     def __len__(self) -> int:
         return len(self.memory)
     
-    def append(self, state, action, reward, done, next_state) -> None:
+    def append(self, states, actions, rewards, dones, next_states) -> None:
         """Добавить опыт в память
         
         """
-        state = torch.tensor(state.__array__())
-        next_state = torch.tensor(next_state.__array__())
-        action = torch.tensor([action])
-        reward = torch.tensor([reward])
-        done = torch.tensor([done])
 
-        if torch.cuda.is_available():
-            # state = state.cuda()
-            # action = action.cuda()
-            # reward = reward.cuda()
-            done = done.cuda()
-            # next_state = next_state.cuda()
-
-        self.memory.append(Experience(state, action, reward, done, next_state))
+        for state, action, reward, done, next_state in zip(states, actions, rewards, dones, next_states):
+            self.memory.append(Experience(state, action, reward, done, next_state))
     
     def sample(self, batch_size: int) -> Tuple:
         """Создать сэмпл
 
         """
         batch = random.sample(self.memory, batch_size)
-        states, actions, rewards, dones, next_states = map(torch.stack, zip(*batch))
-        states, next_states, actions, rewards, dones = states.cuda(), next_states.cuda(), actions.cuda(), rewards.cuda(), dones.cuda()
-        actions = actions.squeeze()
-        rewards = rewards.squeeze()
-        dones = dones.squeeze()
+        states, actions, rewards, dones, next_states = map(np.stack, zip(*batch))
 
-        return states, actions, rewards, dones, next_states
+        states_t = torch.as_tensor(states, dtype=torch.float32, device=self.device)
+        actions_t = torch.as_tensor(actions, dtype=torch.int64, device=self.device).unsqueeze(-1)
+        rewards_t = torch.as_tensor(rewards, dtype=torch.float32, device=self.device).unsqueeze(-1)
+        dones_t = torch.as_tensor(dones, dtype=torch.float32, device=self.device).unsqueeze(-1)
+        next_states_t = torch.as_tensor(next_states, dtype=torch.float32, device=self.device)
+
+        return states_t, actions_t, rewards_t, dones_t, next_states_t
 
 
 class Agent:
-    def __init__(self, env: gym.Env, save_dir: Path = "checkpoints") -> None:
+    def __init__(self, env: gym.Env, device, save_dir: Path = "checkpoints", checkpoint=None) -> None:
         self.env = env
+        self.device = device
         self.save_dir = save_dir
-        self.replay_buffer = ReplayBuffer()
-        self.state_shape = self.env.reset().shape
+        if not checkpoint:
+            self.replay_buffer = ReplayBuffer(device)
+        self.state_shape = self.env.observation_space.shape
         self.action_shape = self.env.action_space.n
         self.batch_size = 32
         self.net = DQN(self.state_shape, self.action_shape)
         if torch.cuda.is_available():
             self.net = self.net.cuda()
+        if checkpoint:
+            self.load(checkpoint)
         self.exploration_rate = 1
-        self.exploration_rate_decay = 0.99999975
+        self.exploration_rate_decay = 0.9999977
         self.exploration_rate_min = 0.1
         self.curr_step = 0
         self.save_every = 5e5
         self.gamma = 0.99 # Discount rate
-        self.burnin = 1e4 # Min experiences before training
+        self.burnin = 5e4 # Min experiences before training
         self.learn_every = 3
-        self.sync_every = 1e4
+        self.sync_every = 1e4 // 4
 
-        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=1e-3)
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=5e-5)
         self.loss_fn = torch.nn.SmoothL1Loss()
 
     def reset(self) -> None:
@@ -83,26 +79,26 @@ class Agent:
         """Используя нейросеть решаем какое действие следует выполнить используя epsilon-greedy политику 
         """
         # Агент исследует среду и выполняет случайное действие
-        if np.random.random() < self.exploration_rate:
-            action = self.env.action_space.sample()
         
         # Агент использует использует накопленный опыт для принятия решения о следующем действии
-        else:
-            state = torch.tensor(state.__array__())
+        obses_t = torch.as_tensor(state, dtype=torch.float32, device=self.device)
+        q_values = self.net(obses_t, model="main")
 
-            if torch.cuda.is_available():
-                state = state.cuda()
+        max_q_indices = torch.argmax(q_values, dim=1)
+        actions = max_q_indices.detach().tolist()
+
+        for i in range(len(actions)):
+            rnd_sample = random.random()
+            if rnd_sample <= self.exploration_rate:
+                actions[i] = random.randint(0, self.action_shape - 1)
             
-            state = state.unsqueeze(0)
-            action_values = self.net(state, model="main")
-            action = torch.argmax(action_values, axis=1).item()
-        
+
         self.exploration_rate *= self.exploration_rate_decay
         self.exploration_rate = max(self.exploration_rate_min, self.exploration_rate)
 
         self.curr_step +=1
 
-        return action
+        return actions
 
     def td_estimate(self, state, action):
         current_Q = self.net(state, model="main")[
@@ -138,6 +134,18 @@ class Agent:
             save_path,
         )
         print(f"Model saved to {save_path} at step {self.curr_step}")
+    
+    def load(self, load_path):
+        if not load_path.exists():
+            raise ValueError(f"{load_path} does not exist")
+
+        ckp = torch.load(load_path, map_location=('cuda' if torch.cuda.is_available() else 'cpu'))
+        exploration_rate = ckp.get('exploration_rate')
+        state_dict = ckp.get('model')
+
+        print(f"Loading model at {load_path} with exploration rate {exploration_rate}")
+        self.net.load_state_dict(state_dict)
+        self.exploration_rate = exploration_rate
     
     def learn(self):
         if self.curr_step % self.sync_every == 0:
